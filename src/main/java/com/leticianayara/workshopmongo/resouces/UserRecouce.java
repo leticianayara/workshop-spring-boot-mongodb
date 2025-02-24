63dd9300ba9b4d3f4717e90b26b16c38d69f39e6
Leticia Costa <leticianayaralima@gmail.com>
2025-02-24 15:41:21 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/leticianayara/workshopmongo/resouces/UserRecouce.java
+++ src/main/java/com/leticianayara/workshopmongo/resouces/UserRecouce.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.leticianayara.workshopmongo.domain.Post;
 import com.leticianayara.workshopmongo.domain.User;
 import com.leticianayara.workshopmongo.dto.UserDTO;
 import com.leticianayara.workshopmongo.services.UserService;
@@ -61,5 +62,11 @@ public class UserRecouce {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
